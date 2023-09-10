95c7d626d14e0616e74c6e897066b53b79413011
h.singh@aexonic.com <mr.technorite@gmail.com>
2023-09-10 18:04:50 -0400

    add constant

--- src/apis/api-constants.ts
+++ src/apis/api-constants.ts
@@ -1,3 +1,3 @@
 export const ImageBaseUrl = 'https://image.tmdb.org/t/p/w300/';
-export const ApiKeyMovies = 'f7ef9e8a7c9615bbf4dac475590d6b00';
+export const ApiKeyMovies = 'your key here';
 export const PopularMovies = `/3/movie/popular?api_key=${ApiKeyMovies}`;
